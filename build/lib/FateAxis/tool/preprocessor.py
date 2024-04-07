e7ccea07a5afc61c19060e67886389d92964832b
jiang_junyao <jiangjunyao789@163.com>
2024-04-07 17:04:21 +0800

    model building

--- build/lib/FateAxis/tool/preprocessor.py
+++ build/lib/FateAxis/tool/preprocessor.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
 """
-Created on Mon Apr  1 15:32:57 2024
+Generate GRP-cell matrix using celloracle
 
-@author: junyao
+author: junyao
 """
 import sys
 import scanpy as sc
@@ -14,11 +13,15 @@ class pper:
     def __init__(self,
                  obj,
                  group,
-                 species='hs',):
+                 baseGRN=None,
+                 species='hs',
+                 ncores=10):
         
         self.adata = obj
-        self.group=group
-        self.species=species
+        self.group = group
+        self.species = species
+        self.ncores = ncores
+
         
         ### set tf db
         if species == 'hs':
@@ -29,10 +32,19 @@ class pper:
                                                       'tfdb/mm_tf.txt')
         else:
             sys.exit("please input correct species name: mm or hs")
-        
+
+        ### use celloracle defualt grn
+        if (baseGRN is None) & (self.species=='hs'):
+            self.baseGRN = co.data.load_human_promoter_base_GRN()
+        elif (baseGRN is None) & (self.species=='mm'):
+            baseGRN = co.data.load_mouse_scATAC_atlas_base_GRN()
+        else:
+            self.baseGRN = baseGRN
+            
     def extract_fea(self,pval_thr=0.05,tf_exp_thr=0.05):
         
         ### extract deg
+        sc.pp.log1p(self.adata)
         sc.tl.rank_genes_groups(self.adata,self.group, method='wilcoxon')
         result = self.adata.uns['rank_genes_groups']
 
@@ -68,18 +80,59 @@ class pper:
         
         ### subset the adata
         self.grn_fea = list(set(differential_genes + selected_tf))
+        print('sig deg num:'+str(len(differential_genes)))
+        print('expressed tf num:'+str(len(selected_tf)))
+        print('total feature num:'+str(len(self.grn_fea)))
         
+    def construct_full_grn(self,embedding_name='X_umap'):
         
+        adata_input = self.adata[:,self.grn_fea]
+        grn = self.run_celloracle(adata_input,self.baseGRN,
+                                  self.group,embedding_name,self.ncores)
+        full_grn = {}
+        full_index = {}
+        for i in grn.links_dict.keys():
+            raw_grn = grn.links_dict[i]
+            filtered_grn = raw_grn[raw_grn['p']<0.05]
+            filtered_grn = filtered_grn[filtered_grn['coef_abs']>0]
+            full_grn[i] = filtered_grn
+            full_index[i] = filtered_grn['source']+'#'+filtered_grn['target']
+            print('celltype '+str(i) + '. raw pair '+str(raw_grn.shape[0])+\
+                  ' filter pair '+str(filtered_grn.shape[0]))
+        self.full_grn = full_grn
+        self.full_index = full_index
+
+    def construct_metacell_grn(self,embedding_name='X_umap',metacell_num=20,
+                               cell_number=100):
+        
+        adata_input = self.adata[:,self.grn_fea]
+        group1 = list(set(adata_input.obs[self.group]))[0]
+        group2 = list(set(adata_input.obs[self.group]))[1]
+        group1_cell = adata_input.obs_names[adata_input.obs[self.group]==group1]
+        group2_cell = adata_input.obs_names[adata_input.obs[self.group]==group2]
+
+        meta_grn_dict = {}
+        for i in range(metacell_num):
+           print(i)
+           group1_meta = np.random.choice(group1_cell, size=cell_number, 
+                                          replace=False).tolist()
+           group2_meta = np.random.choice(group2_cell, size=cell_number, 
+                                          replace=False).tolist()
+           adata_meta = adata_input[group1_meta+group2_meta]
+           sc.pp.filter_genes(adata_meta,min_cells=3)
+           print(adata_meta)
+           meta_grn = self.run_celloracle(adata_meta,self.baseGRN,
+                                          self.group,embedding_name,
+                                          self.ncores)
+           #meta_grn_dict[i] = self.__filter_grp(meta_grn.links_dict)
+           meta_grn_dict[i] = meta_grn.links_dict
+        self.meta_grn = meta_grn_dict
+    
     def run_celloracle(self,adata,baseGRN=None,group='celltype',
-                       embedding_name='X_umap',ncores=10):
+                       embedding_name='X_umap',ncores=10,filter=None):
         
+        print('build grn based on celloracle')
         oracle = co.Oracle()
-        ### use celloracle defualt grn
-        if baseGRN==None & self.species=='hs':
-            baseGRN = co.data.load_human_promoter_base_GRN()
-        elif baseGRN==None & self.species=='mm':
-            baseGRN = co.data.load_mouse_scATAC_atlas_base_GRN()
-            
         oracle.import_anndata_as_raw_count(adata=adata,
                                    cluster_column_name=group,
                                    embedding_name=embedding_name)
@@ -94,39 +147,22 @@ class pper:
                          verbose_level=10)
         return links
 
-    def construct_full_grn(self,embedding_name='X_umap'):
+    def filter_grp(self,grn):
+        filtered_grn = self.meta_grn
+        for i in self.meta_grn.keys():
+            grn_meta = self.meta_grn[i]
+            for j in grn_meta.keys():
+                grn_use = grn_meta[j]
+                grn_use.index = grn_use['source']+'#'+grn_use['target']
+                valid_grp = np.intersect1d(list(grn_use.index),self.full_index[j])
+                grn_use = grn_use.loc[valid_grp]
+                filtered_grn[i][j] = grn_use
+        all_indexes = set(index for d in filtered_grn.values() for sd in d.values() for index in sd.index)
         
-        adata_input = self.adata[:,self.grn_fea]
-        grn = self.run_celloracle(adata_input,self.baseGRN,self.group,embedding_name,self.ncores)
-        full_grn = {}
-        for i in grn.links_dict.keys():
-            raw_grn = grn.links_dict[i]
-            filtered_grn = raw_grn[raw_grn['p']<0.05]
-            full_grn[i] = filtered_grn
-            print('celltype '+str(i) + '. raw pair '+str(raw_grn.shape[0])+' filter pair '+str(filtered_grn.shape[0]))
-        self.full_grn = full_grn
-
-    def construct_metacell_grn(self,embedding_name='X_umap',metacell_num=20,
-                               cell_number=100):
-        
-        adata_input = self.adata[:,self.grn_fea]
-        group1_cell = self.adata.obs_names[self.adata.obs[self.group]=='5']
-        group2_cell = self.adata.obs_names[self.adata.obs[self.group]=='2']
-
-        meta_grn_dict = {}
-        for i in range(metacell_num):
-           print(i)
-           group1_meta = np.random.choice(group1_cell, size=cell_number, 
-                                          replace=False).tolist()
-           group2_meta = np.random.choice(group2_cell, size=cell_number, 
-                                          replace=False).tolist()
-           adata_meta = adata_input[group1_meta+group2_meta]
-           sc.pp.filter_genes(adata_meta,min_cells=3)
-           print(adata_meta)
-           meta_grn = self.run_celloracle(adata_meta,self.baseGRN,self.group,embedding_name,self.ncores)
-           meta_grn_dict[i] = meta_grn.links_dict
-        self.meta_grn = meta_grn_dict
-        
-        
-        
-        
+        result = pd.concat({(str(i)+'#'+str(j)): \
+                            filtered_grn[i][j]['coef_mean'].reindex(all_indexes, 
+                                                                    fill_value=0)
+                            for i in filtered_grn.keys() 
+                            for j in filtered_grn[i].keys()}, 
+                           axis=1)
+        return result
